f4fa9a848f5a349e02d29e6ce117af28d6bd0aa3
Sam Griffith <griffithsamanthaerin@gmail.com>
2022-12-15 14:26:32 -0500

    files cleaned up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; //app is bascially the root where are components will be 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <
+  React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
