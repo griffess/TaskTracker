d97e61ba4f57c5e0859690c0044ea93f957f3c19
Sam Griffith <griffithsamanthaerin@gmail.com>
2022-12-20 14:23:54 -0500

    saveable tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-function App() {
+
+const App = () => { 
+  const [tasks, setTasks ] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Vet Appointment',
+        day: 'May 25th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Anniversary',
+        day: 'July 4th at 5:30pm',
+        reminder: false,
+    },
+]);
+
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+};
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+};
+
+// Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Header />
+        <AddTask onAdd={addTask}/>
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks Yet'}
     </div>
   );
 }
+
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 //rafc - for boiler plate for arrow function component
-const Header = () => {
+import PropTypes from 'prop-types';
+import Button from './Button'
+const Header = ({title}) => {
+
+  const onClick = () => {
+    console.log('Click')
+  }
+
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
 
+Header.defaultProps = {
+    title:'Task Tracker',
+}
+
+Header.prototype ={
+    title: PropTypes.string.isRequired,
+}
 export default Header
