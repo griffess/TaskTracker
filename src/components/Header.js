84fece5b39f10fc0b0f10bc2c2c535abf4f7803b
Sam Griffith <griffithsamanthaerin@gmail.com>
2022-12-20 18:05:41 -0500

    add button collapses/shows task details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 //rafc - for boiler plate for arrow function component
 import PropTypes from 'prop-types';
 import Button from './Button'
-const Header = ({title}) => {
-
-  const onClick = () => {
-    console.log('Click')
-  }
+const Header = ({ title, onAdd }) => {
 
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick} />
+        <Button color='green' text='Add' onClick={onAdd} />
     </header>
   )
 }
